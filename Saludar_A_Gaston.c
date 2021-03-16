4303df860fc34c2f0590de255fcf823451d310f1
Alejo Casas <atlcasas.15@gmail.com>
2021-03-16 16:17:18 -0300

    hooo

--- Saludar_A_Gaston.c
+++ Saludar_A_Gaston.c
@@ -3,7 +3,10 @@ int main(int argc, char const *argv[])
     printf("hola gaston");
     printf("hola  soy gaston");
     printf("hola chicos soy lucas");
+    
     printf("hola chicos soy gastón");
+
     printf("Hola soy el impresentable de siempre");
+
     return 0;
 }
