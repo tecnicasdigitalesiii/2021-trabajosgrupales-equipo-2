bae4ad32e35648336f3808ad94eb9cf4649ab35a
gastonpauta <gaston_pauta@hotmail.com>
2021-03-16 16:03:15 -0300

    vuelto a modificar por gaston

--- Saludar_A_Gaston.c
+++ Saludar_A_Gaston.c
@@ -3,5 +3,7 @@ int main(int argc, char const *argv[])
     printf("hola gaston");
     printf("hola  soy gaston");
     printf("hola chicos soy lucas");
+    printf("hola chicos soy gastón");
+
     return 0;
 }
